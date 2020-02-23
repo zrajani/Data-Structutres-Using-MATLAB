cde21710b9871f63276829bd3919bd7b35448e5a
zrajani <zainrajani2207@gmail.com>
2020-02-22 21:25:01 -0500

    Taylor seris and truncation error concept

---
+++ Taylor_Series_and_Truncation.m
@@ -0,0 +1,13 @@
+% Maclaurin Series for ecp(0.1)
+
+% The code understand Taylor Series and Truncation Error.
+
+n=5;
+a=0.01;
+
+vec=[1:n];
+terms= a.^vec ./ cumprod(vec);
+expVal=1+cumsum(terms);
+
+trueVal=exp(0.01);
+error=abs(trueVal-expVal);
